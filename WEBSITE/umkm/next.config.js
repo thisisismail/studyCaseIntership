3ce1871cb08d48eaae711d0e201ff9d1bc12cbd1
thisisismail <simail.official@gmail.com>
2022-12-23 03:23:50 +0700

    env variable added

--- WEBSITE/umkm/next.config.js
+++ WEBSITE/umkm/next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  env: {
+    apiKey: process.env.APIKEY,
+    authDomain: process.env.AUTHDOMAIN,
+    projectId: process.env.PROJECTID,
+    storageBucket: process.env.STORAGEBUCKET,
+    messagingSenderId: process.env.MESSAGINGSENDERID,
+    appId: process.env.APPID,
+    measurementId: process.env.MEASUREMENTID,
+    databaseURL: process.env.DATABASEURL,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- WEBSITE/umkm/services/firebase/configFirebase.js
+++ WEBSITE/umkm/services/firebase/configFirebase.js
@@ -1,15 +1,14 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 const firebaseConfig = {
-  apiKey: "AIzaSyB_g5BGAwZw8abqURNtHVGXhsb5iPNAKuY",
-  authDomain: "umkm-by-thisisismail.firebaseapp.com",
-  databaseURL:
-    "https://umkm-by-thisisismail-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "umkm-by-thisisismail",
-  storageBucket: "umkm-by-thisisismail.appspot.com",
-  messagingSenderId: "611467175220",
-  appId: "1:611467175220:web:a58f32a8b6e5b70bc1b161",
-  measurementId: "G-PL07YVZ1C4",
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  projectId: process.env.projectId,
+  storageBucket: process.env.storageBucket,
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.appId,
+  measurementId: process.env.measurementId,
+  databaseURL: process.env.databaseURL,
 };
 
 // Initialize Firebase
